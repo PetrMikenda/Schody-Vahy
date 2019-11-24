973937d5769b658460dfdff4e315843ad43cdb38
Petr Mikenda <petr.mikenda@pslib.cz>
2019-11-24 20:10:16 +0100

    dodělání hlavní fuknce porgramu

--- Váhy/Program.cs
+++ Váhy/Program.cs
@@ -9,13 +9,22 @@ namespace Váhy
     class Program
     {
         static void Main(string[] args)
-        { 
+        {
             //Vaha v1 = new Vaha(2, 10);
             //v1.weight();
             ConsoleKeyInfo ans;
             do
             {
-                Console.WriteLine("Chceš vypsat další váhy? [A]");
+                string power;
+                string count;
+                Console.WriteLine("Zadej základ soustavy: ");
+                power = Console.ReadLine();
+                int x = Int32.Parse(power);
+                Console.WriteLine("Zadej počet vah: ");
+                count = Console.ReadLine();
+                int y = Int32.Parse(count);
+                Vaha v1 = new Vaha(x, y);
+                Console.WriteLine("\nChceš vypsat další váhy? [A]");
                 ans = Console.ReadKey();
             } while (ans.Key == ConsoleKey.A || ans.Key == ConsoleKey.Enter);
 
--- Váhy/Vaha.cs
+++ Váhy/Vaha.cs
@@ -8,18 +8,29 @@ namespace Váhy
 {
     class Vaha
     {
-        private int _basis, _count;
-        double[] vahy = new double[50];
+        private int _basis, _count, x;
+        int[] vahy = new int[31];
         public Vaha(int basis, int count)
         {
             _basis = basis;
             _count = count;
+            x = _count - 1;
+            for (int i = 0; i < _count; i++, x--)
+            {
+                vahy[i] = Convert.ToInt32(Math.Pow(_basis, x));
+            }
+            Console.Write("Základ soustavy: " + _basis + " Váhy: ");
             for (int i = 0; i < _count; i++)
             {
-                vahy[i] = Math.Pow(_basis, i);
+                Console.Write(vahy[i] + "; ");
             }
-            
+
         }
+        //public static get()
+        //{
+        //    return 
+        //}
+
         //public void weight()
         //{
         //    for (int i = 0; i < _count; i++)
